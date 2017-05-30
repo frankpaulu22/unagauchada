5569483ebddcbfe425d2ffda523cdaadd79dc0d0
DiazLighuen <lighuen.diaz@gmail.com>
2017-05-29 22:13:56 -0300

    Add files via upload
    modificaciones esteticas. se quito de las gauchadas el campo de E-mail.

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 
         <?php 
             include("php/menu.php"); 
-            session_start(); 
-
-            if ($_SESSION['estado']== 'logeado'){
+            
+            if ( isset($_SESSION['estado']) && $_SESSION['estado']== 'logeado'){
                 ?><div id='publicar'><a href="php/gauchada/nueva.php?usid=<?php echo $varuser['id_usuario']; ?>" >Publicar</a></div><?php
             }
 
@@ -25,12 +24,11 @@
                 ?>
                 <div id='elemento'>
                 <?php
-                    ?><div id='eletitulo'>Titulo: <?php echo $lista['titulo']; ?></div><?php
+                    ?><div id='eletitulo'><?php echo $lista['titulo']; ?></div><?php
                     ?><div id='elecategoria'>Categoria: <?php echo $lista['categoria']; ?></div><?php
-                    ?><div id='eleemail'>De: <?php echo $lista['email']; ?></div><?php
                     ?><div id='eleciudad'>En: <?php echo $lista['ciudad']; ?></div><?php
                     ?><div id='eledescripcion'><?php echo $lista['descripcion']; ?></div><?php
-                    ?><div id="eleimagen"><img height="120px" width="354px" src="data:<?php echo $lista['extension']; ?>;base64,<?php echo base64_encode($lista['foto']); ?>"/></div><?php
+                    ?><div id="eleimagen"><img height="120px" src="data:<?php echo $lista['extension']; ?>;base64,<?php echo base64_encode($lista['foto']); ?>"/></div><?php
                     if (isset($_SESSION['estado']) && $_SESSION['estado']== 'logeado'){
                         ?><div id='eledetalle'><a href="php/gauchada/detalle.php?ga=<?php echo $lista['id_gauchada']; ?>" >Detalle</a></div><?php
                     }
