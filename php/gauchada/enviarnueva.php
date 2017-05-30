c8362a4cce4bf97eaee2401b9ed3e96d7122b374
frankpaulu22 <frankpaulu22@gmail.com>
2017-05-30 05:24:05 -0300

    Se acomodo el filtro para imagenes

--- php/gauchada/enviarnueva.php
+++ php/gauchada/enviarnueva.php
@@ -1,15 +1,43 @@
 <?php
 	if(isset($_POST['titulo'])){
 		include('../conexion.php');
-
+		$img = array('image/jpg', 'image/jpeg', 'image/png');
 		$titulo= $_POST['titulo'];
 		$categoria= $_POST['categoria'];
 		$ciudad= $_POST['ciudad'];
 		$descripcion= $_POST['descripcion'];
 		$usrid= $_POST['userid'];
 		if($_FILES['imagen']['tmp_name']!=""){
-			$imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+		    if($_FILES['imagen']['size'] > 500000) {
+		        ?>
+		        <script>
+		            alert('El tama\u00f1o de la imagen es muy grande');
+		            window.location.href="php/gauchada/nueva.php?usid=<?php echo $usrid; ?>";
+		        </script>
+		        <?php
+		    }
+		    else {
+		        foreach($img as $extension) {
+		            if($_FILES['imagen']['type'] == $extension) {
+		                $extvalida=1;
+		                break;
+		            }
+		            else {
+		                $extvalida=0;
+		            }
+		        }
+		    }
+		    if($extvalida == 0) {
+		        ?>
+		        <script>
+		            alert('La extension de la imagen no es un tipo de imagen valido');
+		            window.location.href='php/gauchada/nueva.php?usid=<?php echo $usrid; ?>';
+		        </script>
+		        <?php
+		    }
+		    $imagen = addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
 		    $extension = $_FILES['imagen']['type'];
+
 		}
 		else {
 			$ruta= "logo.png";
