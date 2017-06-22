4a01f34bf5eac6eb7db9d3034194b56ff1303ee6
DiazLighuen <lighuen.diaz@gmail.com>
2017-06-22 05:20:36 +0200

    corrección de campo foto por foto1

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
                     <div id='elecategoria'>Categoria: <?php echo $lista['categoria']; ?></div>
                     <div id='eleciudad'>En: <?php echo $lista['ciudad']; ?></div>
                     <div id='eledescripcion'><?php echo $lista['descripcion']; ?></div>
-                    <div id="eleimagen"><img height="240px" src="data:<?php echo $lista['extension']; ?>;base64,<?php echo base64_encode($lista['foto']); ?>"/></div><?php
+                    <div id="eleimagen"><img height="240px" src="data:<?php echo $lista['extension']; ?>;base64,<?php echo base64_encode($lista['foto1']); ?>"/></div><?php
                     if (isset($_SESSION['estado']) && $_SESSION['estado']== 'logeado'){
                         ?><div id='eledetalle'><a href="php/gauchada/detalle.php?ga=<?php echo $lista['id_gauchada']; ?>" >Detalle</a></div><?php
                     }
