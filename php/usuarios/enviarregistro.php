b0468a4f1af9bcd091cd0dce2b0f9e90e61d4a97
DiazLighuen <lighuen.diaz@gmail.com>
2017-06-26 05:40:08 +0200

    actualizacion (Enviar registro tiene problemas)

--- php/usuarios/enviarregistro.php
+++ php/usuarios/enviarregistro.php
@@ -29,12 +29,12 @@
 		$clave = $_POST['clave'];
 		$fecha = $_POST['fecha'];
 		$telefono = $_POST['telefono'];
-        $direc = 'usuario.png';
+        $direc = "usuario.png";
         $foto = addslashes(file_get_contents($direc));
-        $extension = "image/png";
-
+        $extension = "image/png";        
+        
 		$insertar = "INSERT INTO usuarios(nombre, apellido, email, clave, nacimiento, telefono, foto, extension) VALUES ('$nombre', '$apellido', '$email', '$clave', '$fecha', '$telefono', '$foto', '$extension')";
-	    $resultado = mysqli_query($conexion, $insertar) or die ('Problemas en la consulta'. mysql_error());
+	    $resultado = mysqli_query($conexion, $insertar) or die ('Problemas en la consulta2'. mysql_error());
 
 	?>   
 	    <script>
