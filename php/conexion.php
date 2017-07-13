c7db2d648c0500342c39b9b798fbd71b73b15be8
frankpaulu22 <frankpaulu22@gmail.com>
2017-07-13 01:25:35 -0300

    Arreglado el problema con la muestra de caracteres especiales.

--- php/conexion.php
+++ php/conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = new mysqli('localhost', 'root', 'pansa', 'gauchadas.com') or die ('Problemas en la conexion'. mysql_error());
-$conexion->set_charset("utf8");
+    $conexion = new mysqli('localhost', 'root', '', 'gauchadas.com') or die ('Problemas en la conexion'. mysql_error());
+ $conexion->set_charset("utf8");
 ?>
